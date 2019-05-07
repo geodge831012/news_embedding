4105ba3ead66cf7e02c3badb6e701b95307c5fb8
geodge831012 <geodge831012@163.com>
2019-05-07 09:55:41 +0800

    优化代码

--- func_utils.pyx
+++ func_utils.pyx
@@ -1,5 +1,4 @@
 # coding:utf-8
-#import math
 
 def get_cosine_value(X_list, Y_list, X_norm, Y_norm):
 
@@ -12,7 +11,9 @@ def get_cosine_value(X_list, Y_list, X_norm, Y_norm):
     if (X_norm <= 0.0 or Y_norm <= 0.0 or len(X_list) != len(Y_list)):
         return 0
 
-    X = X_list.reshape(1, 256)
-    Y = Y_list.reshape(1, 256)
+    XY_size = len(X_list)   #XY_size = 256
+
+    X = X_list.reshape(1, XY_size)
+    Y = Y_list.reshape(1, XY_size)
 
     return float(X.dot(Y.T) / (X_norm * Y_norm))
